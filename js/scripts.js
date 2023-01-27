e4a9ae147d5c22af9bfce0feab5698f330bf03cd
jaceyh <jacey.hallock@gmail.com>
2023-01-27 12:37:18 -0500

    add loadList function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,44 @@
 let artRepository = (function() {
 
+    let artList = []
+
+    let apiUrl = "https://api.artic.edu/api/v1/artworks?limit=20#"
+
+    //GET list from API
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.data.forEach(function (item) {
+                let artwork = {
+                    title: item.title,
+                    artist: item.artist_titles,
+                    img: item.thumbnail,
+                    date: item.date_display,
+                    location: item.place_of_origin,
+                };
+                console.log(artwork);
+              //  add(artwork);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
 
 
+    //GET details from API
+
+
+
+    //add artwork to list
+
+
+    return {
+        loadList: loadList,
+    };
 
 
 //end of IIFE function
 })();
+
+artRepository.loadList();
