107e6690809a321d2ad939c55765bccaebb8383e
jaceyh <jacey.hallock@gmail.com>
2023-01-30 16:57:03 -0500

    create and format addListItem

--- js/scripts.js
+++ js/scripts.js
@@ -17,28 +17,55 @@ let artRepository = (function() {
                     date: item.date_display,
                     location: item.place_of_origin,
                 };
-                console.log(artwork);
-              //  add(artwork);
+                add(artwork);
             });
         }).catch(function (e) {
             console.error(e);
         })
     }
 
+/*
+    
+    let artist= [];
+    artwork.artist_titles.forEach(function (artist_titles) {
+        JSON.parse(artist_titles);
+        console.log(artist_titles);
+    })
+*/
 
-    //GET details from API
-
-
+    //function to return all items in artList
+        function getAll (){
+            return artList;
+        }
 
     //add artwork to list
+    function add (artwork) {
+        artList.push(artwork);
+    }
 
+
+    function addListItem (artwork) {
+        let container = document.querySelector('.artwork-list');
+        let button = document.createElement('button');
+        button.innerText = artwork.title;
+        button.classList.add("button-class");
+        container.appendChild(button);
+    }
 
     return {
+        getAll: getAll,
+        add: add,
         loadList: loadList,
+        addListItem: addListItem,
     };
 
 
 //end of IIFE function
 })();
 
-artRepository.loadList();
+artRepository.loadList().then(function() {
+    //now the data is loaded!
+    artRepository.getAll().forEach(function (artwork) {
+        artRepository.addListItem(artwork);
+    });
+});
